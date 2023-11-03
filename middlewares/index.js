26967bdf5d6e58d1f9d2ba1b8d90f52c4824275f
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-11-03 19:58:18 +0100

    add garvatar

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validate");
 const isValidId = require("./isValidId")
+const upload = require("./upload");
 
 const authenticate = require("./authenticate")
 module.exports={
     validateBody,
     isValidId,
     authenticate,
+    upload,
 }
